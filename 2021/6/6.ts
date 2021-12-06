3475e53d5af0e88b47195856b6d17cccdfe29bed
Trevor O <trevor@trevoro.net>
2021-12-06 14:20:00 -0800

    getting ready

--- 2021/6/6.ts
+++ 2021/6/6.ts
@@ -1,25 +1,30 @@
 // @format
+//
+const numTicks = 256;
 
 const input = Deno.readTextFileSync('./input/6')
   .split(',')
   .map(e => parseInt(e, 10)) as number[];
 
-const fish = input.reduce((p: number[], c) => {
-  p[c]++;
-  return p;
-}, new Array(9).fill(0));
-
-const _main = () => {
-  for (let i = 0; i < 256; i++) {
-    tick();
-  }
-  console.log(fish.reduce((p, c) => p + c));
+const school = (input: number[]): number[] => {
+  return input.reduce((p: number[], c) => {
+    p[c]++;
+    return p;
+  }, new Array(9).fill(0));
 };
 
-const tick = () => {
+const tick = (fish: number[]) => {
   const x = fish.shift() as number;
   fish[8] = x;
   fish[6] += x;
+};
+
+const _main = () => {
+  const fish = school(input);
+  for (let i = 0; i < numTicks; i++) {
+    tick(fish);
+  }
+  console.log(fish.reduce((p, c) => p + c));
 };
 
 _main();
---
+++ 2021/7/7.ts
@@ -0,0 +1,9 @@
+// @format
+
+const input = Deno.readTextFileSync('./input/7_test');
+
+const _main = () => {
+  console.log(input);
+};
+
+_main();
