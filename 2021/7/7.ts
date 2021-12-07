e35b9f997919b0cf0ac1c089d9f66ad7f679dcaa
Trevor O <trevor@trevoro.net>
2021-12-07 12:14:30 -0800

    brute

--- 2021/7/7.ts
+++ 2021/7/7.ts
@@ -1,9 +1,32 @@
 // @format
+//
+//
+// brute force today
 
-const input = Deno.readTextFileSync('./input/7_test');
+const read = () => {
+  return Deno.readTextFileSync('./input/7')
+    .split(',')
+    .map(e => parseInt(e, 10));
+};
+
+const fuel = (n: number) => {
+  let rval = 1;
+  for (let i = 2; i <= n; i++) rval = rval + i;
+  return rval;
+};
+
+const min = (input: number[]) => {
+  input.sort((a, b) => a - b);
+  const max = input.at(-1) as number;
+  const set = [...Array(max).keys()].map(i => {
+    return input.map(e => fuel(Math.abs(e - i))).reduce((p, c) => p + c);
+  });
+  return Math.min(...set);
+};
 
 const _main = () => {
-  console.log(input);
+  const input = read();
+  console.log(min(input));
 };
 
 _main();
